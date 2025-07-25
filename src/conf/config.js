27d096abdf9cd60ab1f7b6fa63a22901dfd8a398
Ariful Islam <remalarif0130@gmail.com>
2025-07-25 16:20:03 +0600

    auth done

---
+++ src/conf/config.js
@@ -0,0 +1,9 @@
+const conf ={
+    appwriteUrl: String (import.meta.env.VITE_APP_APPWRITRE_URL),
+    appwriteProjectId: String (import.meta.env.VITE_APP_PROJECT_ID),
+    appwriteDatabseId: String (import.meta.env.VITE_APP_DATABASE_ID),
+    appwriteCollectionId: String (import.meta.env.VITE_APP_COLLECTION_ID),
+    appwriteBucketId: String (import.meta.env.VITE_APP_BUCKET_ID),
+}
+
+export default conf;
